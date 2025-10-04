edaa4fd108dc06fdbca3f0827b9dd72df551d529
IsBebs <banangurkkungen@gmail.com>
2025-10-04 15:14:18 +0200

    player attack does now do a collision check

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,8 +9,9 @@ using UnityEngine.InputSystem;
 public class PlayerAttack : MonoBehaviour
 {
 
+    const string ghostTag = "Ghost";
 
-     [SerializeField]
+    [SerializeField]
     PlayerInput playerInput;
 
     InputActionMap actionMap;
@@ -16,10 +20,16 @@ public class PlayerAttack : MonoBehaviour
 
 
     [SerializeField]
-    GameObject shootHitboxParent;
+    GameObject suckHitboxParent;
 
     [SerializeField]
-    Collider2D shootHitbox;
+    Collider2D suckHitbox;
+
+    [SerializeField]
+    LayerMask ghostLayerMask;
+
+    [SerializeField]
+    LayerMask ghostAndWallLayerMask;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,42 +46,70 @@ public class PlayerAttack : MonoBehaviour
         {
             Debug.Log(attackDirection);
 
-            Quaternion parentRotation = shootHitboxParent.transform.rotation;
+            Quaternion parentRotation = suckHitboxParent.transform.rotation;
+
+            #region  set attack hitbox rotation
 
             if (attackDirection.x > 0.5f && attackDirection.y > 0.5f)
-            { 
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 135.0f * Mathf.Deg2Rad));
+            {
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 135.0f * Mathf.Deg2Rad));
             }
-            else   if (attackDirection.x > 0.5f && attackDirection.y < -0.5f)
-            { 
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 45.0f * Mathf.Deg2Rad));
+            else if (attackDirection.x > 0.5f && attackDirection.y < -0.5f)
+            {
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 45.0f * Mathf.Deg2Rad));
             }
-            else   if (attackDirection.x < -0.5f && attackDirection.y > 0.5f)
-            { 
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 225.0f * Mathf.Deg2Rad));
+            else if (attackDirection.x < -0.5f && attackDirection.y > 0.5f)
+            {
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 225.0f * Mathf.Deg2Rad));
             }
-            else   if (attackDirection.x < -0.5f && attackDirection.y < -0.5f)
-            { 
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 315.0f * Mathf.Deg2Rad));
+            else if (attackDirection.x < -0.5f && attackDirection.y < -0.5f)
+            {
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 315.0f * Mathf.Deg2Rad));
             }
             else if (attackDirection.x > 0.5f)
             {
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 90.0f * Mathf.Deg2Rad));
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 90.0f * Mathf.Deg2Rad));
             }
             else if (attackDirection.x < -0.5f)
             {
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 270.0f * Mathf.Deg2Rad));
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 270.0f * Mathf.Deg2Rad));
             }
             else if (attackDirection.y > 0.5f)
             {
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 180.0f * Mathf.Deg2Rad));
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 180.0f * Mathf.Deg2Rad));
             }
             else if (attackDirection.y < -0.5f)
             {
-                shootHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 0.0f * Mathf.Deg2Rad));
+                suckHitboxParent.transform.rotation = quaternion.Euler(new float3(parentRotation.x, parentRotation.y, 0.0f * Mathf.Deg2Rad));
             }
-            
 
+            #endregion
+            List<Collider2D> overlapedColliders = new List<Collider2D>();
+
+            ContactFilter2D ghostFilter = new ContactFilter2D();
+            ghostFilter.layerMask = ghostLayerMask;
+            int hits = Physics2D.OverlapCollider(suckHitbox, ghostFilter,overlapedColliders);
+            if (hits > 0)
+            {
+                ContactFilter2D ghostAndWallFilter = new ContactFilter2D();
+                ghostAndWallFilter.layerMask = ghostAndWallLayerMask;
+                Vector2 playerPosition = transform.position;
+                foreach (Collider2D overlapedCollider in overlapedColliders)
+                {
+                    Vector2 enemyPosition = overlapedCollider.transform.position;
+                    Vector2 enemyPlayerDelta = playerPosition - enemyPosition;
+                    RaycastHit2D raycastHit2D = Physics2D.Raycast(playerPosition, enemyPlayerDelta.normalized, enemyPlayerDelta.magnitude);
+                    if (raycastHit2D)
+                    {
+                        bool isGhost = raycastHit2D.collider.gameObject.CompareTag(ghostTag);
+                        if (isGhost)
+                        { 
+                            //attack here
+                        }
+                    }
+
+                }
+            }
         }
         // 
     }
